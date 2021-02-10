130f623e3d788cabd2b803d93f6913e30612f585
yuriy <yuralucky83@gmail.com>
2021-02-10 21:11:11 +0200

    last

--- public/index.php
+++ public/index.php
@@ -7,36 +7,43 @@ use Hillel\Model\Tag;
 require_once '../vendor/autoload.php';
 require_once '../config/eloquent.php';
 
-for ($i = 0; $i < 6; $i++) {
-    $category = new Category();
-    $category->name = 'Category  №' . $i;
-    $category->save();
-}
+//for ($i = 0; $i < 6; $i++) {
+//    $category = new Category();
+//    $category->title = 'Category  №' . $i;
+//    $category->slug = 'Category  №' . $i;
+//    $category->save();
+//}
 
-$category = Category::find(4);
-$category->name = 'new category';
-$category->update();
-$category->delete();
+//$category = Category::find(4);
+////$category->title = 'new category';
+////$category->update();
+//$category->delete();
+//
+//for ($i = 1; $i <= 10; $i++) {
+//    $post = new Post();
+//    $post->title = 'title №' . rand(1, 100);
+//    $post->slug = 'slug-' . rand(1, 100);
+//    $post->body = 'body' . rand(1, 100);
+//    $category = Category::all()->random();
+//    $category->posts()->save($post);
+//}
+$post = Post::find(1);
+$post->title = 'Another';
+$category = Category::all()->random();
+$category->posts()->update($post);
 
-for ($i = 1; $i <= 10; $i++) {
-    $post = new Post();
-    $post->title = 'title №' . rand(1, 100);
-    $post->body = 'body №' . rand(1, 100);
-    $category = Category::all()->random();
-    $category->posts()->save($post);
-}
-
-for ($i = 1; $i <= 10; $i++) {
-    $tag = new Tag();
-    $tag->name = 'name' . rand(1, 100);
-    $tag->save();
-}
-$tags = Tag::all();
-
-foreach ($tags as $tag) {
-    for ($i = 1; $i < 4; $i++) {
-        $tag->posts()->attach(Post::all()->random());
-    }
-}
+//
+//for ($i = 1; $i <= 10; $i++) {
+//    $tag = new Tag();
+//    $tag->name = 'name' . rand(1, 100);
+//    $tag->save();
+//}
+//$tags = Tag::all();
+//
+//foreach ($tags as $tag) {
+//    for ($i = 1; $i < 4; $i++) {
+//        $tag->posts()->attach(Post::all()->random());
+//    }
+//}
 
 
